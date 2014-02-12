3896aae8151d081167037d45f26eccde0c0c7225
Mr_okushama <r-okushama@hotmail.com>
2014-02-12 14:16:43 +0000

    Remembered how to spell my name

--- okushama/poku/TickRender.java
+++ okushama/poku/TickRender.java
@@ -29,7 +29,7 @@ public class TickRender extends Ticker {
 			line = new String[] { "PAUSED" };
 		}
 		if (!Game.gameStarted) {
-			line = new String[] { "---POKI---", "","","","PRESS", "SPACE KEY" };
+			line = new String[] { "---POKU---", "","","","PRESS", "SPACE KEY" };
 		}
 		if (Game.gameFinished) {
 			if (Game.winner == 1) {
